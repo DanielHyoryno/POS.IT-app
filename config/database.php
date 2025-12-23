7c9506cb1048835dee4f3d621fd9dd35346250cb
Daniel Hyoryno Thenaka <danielhyorynothenaka@gmail.com>
2025-12-23 18:32:15 +0700

    Fix Aiven SSL CA for Vercel

--- config/database.php
+++ config/database.php
@@ -1,65 +1,36 @@
 <?php
 
 use Illuminate\Support\Str;
-use PDO;
 
-return [
+$sslOptions = [];
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Database Connection Name
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for database operations. This is
-    | the connection which will be utilized unless another connection
-    | is explicitly specified when you execute a query / statement.
-    |
-    */
+if (extension_loaded('pdo_mysql')) {
+    $pem = env('MYSQL_ATTR_SSL_CA');
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Below are all of the database connections defined for your application.
-    | An example configuration is provided for each database system which
-    | is supported by Laravel. You're free to add / remove connections.
-    |
-    */
-
-    'options' => (function () {
-        if (!extension_loaded('pdo_mysql')) {
-            return [];
-        }
-
-        $pem = env('MYSQL_ATTR_SSL_CA');
-
-        if (!$pem) {
-            return [];
-        }
-
-        // If it's PEM text, write to a temp file for PDO
+    if ($pem) {
+        // If PEM text, write to temp file (Vercel writable: /tmp)
         if (str_contains($pem, 'BEGIN CERTIFICATE')) {
+            $pem = str_replace(["\r\n", "\r"], "\n", $pem);
             $path = sys_get_temp_dir() . '/aiven-ca.pem';
             file_put_contents($path, $pem);
 
-            return [
-                PDO::MYSQL_ATTR_SSL_CA => $path,
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            $sslOptions = [
+                \PDO::MYSQL_ATTR_SSL_CA => $path,
+                \PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            ];
+        } else {
+            // If it's already a file path
+            $sslOptions = [
+                \PDO::MYSQL_ATTR_SSL_CA => $pem,
+                \PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ];
         }
+    }
+}
 
-        // If user provided a file path instead
-        return [
-            PDO::MYSQL_ATTR_SSL_CA => $pem,
-            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-        ];
-    })(),
+return [
 
-
+    'default' => env('DB_CONNECTION', 'sqlite'),
 
     'connections' => [
 
@@ -89,9 +60,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => $sslOptions,
         ],
 
         'mariadb' => [
@@ -109,9 +78,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => $sslOptions,
         ],
 
         'pgsql' => [
@@ -146,32 +113,10 @@ return [
 
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Migration Repository Table
-    |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run on the database.
-    |
-    */
-
     'migrations' => [
         'table' => 'migrations',
         'update_date_on_publish' => true,
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
-    | such as Memcached. You may define your connection settings here.
-    |
-    */
 
     'redis' => [
 
